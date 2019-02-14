73a1d1dfcdef84c5012a51652634add1b1ebe49f
Александр Ящук <a.yashuk@pflb.ru>
2019-02-14 13:30:11 +0300

    Заготовка файла конфигурации "gradle.properties", базового и Смоки тестовых классов.

---
+++ src/test/java/ru/performancelab/tests/BaseTest.java
@@ -0,0 +1,12 @@
+package ru.performancelab.tests;
+
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
+public abstract class BaseTest {
+
+    protected static final Logger log = LogManager.getLogger();
+
+}
---
+++ src/test/java/ru/performancelab/tests/suites/Smoke/Smoke.java
@@ -0,0 +1,13 @@
+package ru.performancelab.tests.suites.Smoke;
+
+
+import org.testng.annotations.Test;
+import ru.performancelab.tests.BaseTest;
+
+
+@Test(groups = {"Smoke"})
+public class Smoke extends BaseTest {
+
+
+
+}
